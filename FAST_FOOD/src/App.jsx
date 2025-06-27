0243616e6efac3e35d3c9755998c5e4a31e2ef29
faydzak <fairuzdzak05@gmail.com>
2025-06-27 23:50:33 +0800

    initial commit

---
+++ FAST_FOOD/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SEACateringHomepage from './components/SEACateringHomepage'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <SEACateringHomepage />
+    </div>
+  )
+}
+
+export default App
